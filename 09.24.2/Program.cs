d31c62061f1e79e4a103d0ce252cf966d03cab00
b3kabdom <b3kabdom@vasvari.org>
2026-09-24 21:16:42 +0200

    kesz

--- 09.24.2/Program.cs
+++ 09.24.2/Program.cs
@@ -23,21 +23,48 @@ namespace _09._24._2
             Nationality = adatok[2];
             try
             {
-                
+                birthPlace = adatok[3];
             }
-            birthDate = DateTime.Parse(adatok[4]);
+            catch (Exception)
+            {
+                birthPlace = "";
+            }
+            birthDate = DateTime.TryParse(adatok[4], out DateTime tempDate) ? tempDate : DateTime.MinValue;
         }
 
     }
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main()
+        {
+            Feladat();
+        }
+        static void Feladat()
         {
             List<Kosarasok> data = File.ReadAllLines("kosarasok.csv").Skip(1).Select(ln => new Kosarasok(ln)).ToList();
 
             Console.WriteLine($"3. feladat: {data.Count()}");
-            Console.WriteLine($"4. feladat: {string.Join("\t\n", data.Where(x => x.Nationality == "USA" && x.birthPlace == "").Select(x => $"{x.Name}"))}");
 
+            Console.WriteLine($"4. feladat: \t\n{string.Join("\t\n", data.Where(x => x.Nationality == "USA" && x.birthPlace == "").Select(x => $"{x.Name}"))}");
+
+            Console.WriteLine("5. feladat: Adjon meg egy évszámot 1970 és 2000 között!");
+
+            int year;
+
+            while (true)
+            {
+                year = Convert.ToInt32(Console.ReadLine());
+                if (year >= 1970 && year <= 2000)
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Hibás adat, kérek egy 1970 és 2000 közötti számot!");
+                }
+            }
+
+            Console.WriteLine($"6. feladat: \t\n{data.Count(x => x.birthDate.Year == year)} fő");
         }
     }
 }
